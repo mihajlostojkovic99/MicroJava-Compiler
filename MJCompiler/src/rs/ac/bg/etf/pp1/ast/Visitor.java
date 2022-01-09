2f1d551dfe0162078447d5a4c03c26db254f6c59
Mihajlo Stojkovic <70921912+mihajlostojkovic99@users.noreply.github.com>
2022-01-09 19:52:22 +0100

    wip

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/Visitor.java
@@ -0,0 +1,12 @@
+// generated with ast extension for cup
+// version 0.8
+// 9/0/2022 19:48:58
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public interface Visitor { 
+
+    public void visit(Program Program);
+
+}
