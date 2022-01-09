096518926034270535007a4e191964801a380e93
Mihajlo Stojkovic <70921912+mihajlostojkovic99@users.noreply.github.com>
2022-01-09 20:13:17 +0100

    wip

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/RuleVisitor.java
@@ -0,0 +1,16 @@
+package rs.ac.bg.etf.pp1;
+
+import org.apache.log4j.Logger;
+import rs.ac.bg.etf.pp1.ast.*;
+
+public class RuleVisitor extends VisitorAdaptor{
+
+	int ProgramCount = 0;
+	
+	Logger log = Logger.getLogger(getClass());
+
+	public void visit(Program Program) {
+		ProgramCount++;
+	}
+
+}
